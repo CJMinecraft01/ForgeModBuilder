831f39fefb06e57900ce7b036a9f2051b79a947b
CJMinecraft01 <speed2pc2@gmail.com>
2018-06-18 19:45:42 +0100

    Altered languages location in Language Manager

--- ForgeModBuilder2/Managers/LanguageManager.cs
+++ ForgeModBuilder2/Managers/LanguageManager.cs
@@ -18,7 +18,7 @@ namespace ForgeModBuilder.Managers
         {
             if (ForgeModBuilder.Debugging)
             {
-                LanguagesFilePath = @"..\..\Languages\";
+                LanguagesFilePath = @"..\..\..\Languages\";
             }
             
             if (File.Exists(LanguagesFilePath + "languages.json"))

2ae5efec972ceb0ca944226a68f823a99eb93f10
CJMinecraft01 <speed2pc2@gmail.com>
2018-07-04 12:01:29 +0100

    Organise files

---
+++ ForgeModBuilder2/Gradle/GradleObjects.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+
+namespace ForgeModBuilder.Gradle
+{
+
+    public class GObject
+    {
+        public string Name { get; set; }
+        public int NestedLevel { get; set; } = -1;
+
+        public override string ToString()
+        {
+            return GetTab() + Name;
+        }
+
+        protected string GetTab()
+        {
+            string tab = "";
+            if (NestedLevel > 0)
+            {
+                tab = new string(' ', NestedLevel * 4);
+            }
+            return tab;
+        }
+    }
+
+    public class GVariable : GObject
+    {
+        public object Value { get; private set; }
+
+        public GVariable(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return GetTab() + Name + " = " + Value;
+        }
+    }
+
+    public class GBlock : GObject
+    {
+        public List<GObject> Children { get; private set; } = new List<GObject>();
+
+        public override string ToString()
+        {
+            string tab = GetTab();
+            string text = tab + Name + " {\n";
+            foreach (GObject child in Children)
+            {
+                text += child + "\n";
+            }
+            text += tab + "}";
+            return text;
+        }
+    }
+
+    public class GTask : GBlock
+    {
+        public GTask(GBlock block)
+        {
+            Name = block.Name;
+            Children.AddRange(block.Children);
+            NestedLevel = block.NestedLevel;
+        }
+
+        public override string ToString()
+        {
+            string tab = GetTab();
+            string text = tab + "task " + Name + " {\n";
+            foreach (GObject child in Children)
+            {
+                text += child + "\n";
+            }
+            text += tab + "}";
+            return text;
+        }
+    }
+
+    public static class GradleExtensions
+    {
+        public static bool ContainsObject(this List<GObject> list, string name)
+        {
+            foreach (GObject child in list)
+            {
+                if (child.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static GObject GetObject(this List<GObject> list, string name)
+        {
+            foreach (GObject child in list)
+            {
+                if (child.Name == name)
+                {
+                    return child;
+                }
+            }
+            return null;
+        }
+    }
+
+}
--- ForgeModBuilder2/Gradle/GradleParser.cs
+++ ForgeModBuilder2/Gradle/GradleParser.cs
@@ -1,16 +1,10 @@
-﻿using ForgeModBuilder.Gradle;
-using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace ForgeModBuilder.Gradle
 {
-    // TODO Finish reader
-    public static class GradleReader
+    public static class GradleParser
     {
         public static GBlock ReadBuildFile(string Path)
         {
@@ -290,110 +284,6 @@ namespace ForgeModBuilder.Gradle
             else if (chunk.StartsWith("\'") && chunk.EndsWith("\'"))
             {
                 return chunk.Substring(1, chunk.Length - 2);
-            }
-            return null;
-        }
-
-    }
-
-    public class GObject
-    {
-        public string Name { get; set; }
-        public int NestedLevel { get; set; } = -1;
-
-        public override string ToString()
-        {
-            return GetTab() + Name;
-        }
-
-        protected string GetTab()
-        {
-            string tab = "";
-            if (NestedLevel > 0)
-            {
-                tab = new string(' ', NestedLevel * 4);
-            }
-            return tab;
-        }
-    }
-
-    public class GVariable : GObject
-    {
-        public object Value { get; private set; }
-
-        public GVariable(string name, object value)
-        {
-            Name = name;
-            Value = value;
-        }
-
-        public override string ToString()
-        {
-            return GetTab() + Name + " = " + Value;
-        }
-    }
-
-    public class GBlock : GObject
-    {
-        public List<GObject> Children { get; private set; } = new List<GObject>();
-
-        public override string ToString()
-        {
-            string tab = GetTab();
-            string text = tab + Name + " {\n";
-            foreach (GObject child in Children)
-            {
-                text += child + "\n";
-            }
-            text += tab + "}";
-            return text;
-        }
-    }
-
-    public class GTask : GBlock
-    {
-        public GTask(GBlock block)
-        {
-            Name = block.Name;
-            Children.AddRange(block.Children);
-            NestedLevel = block.NestedLevel;
-        }
-
-        public override string ToString()
-        {
-            string tab = GetTab();
-            string text = tab + "task " + Name + " {\n";
-            foreach (GObject child in Children)
-            {
-                text += child + "\n";
-            }
-            text += tab + "}";
-            return text;
-        }
-    }
-
-    public static class GradleExtensions
-    {
-        public static bool ContainsObject(this List<GObject> list, string name)
-        {
-            foreach (GObject child in list)
-            {
-                if (child.Name == name)
-                {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        public static GObject GetObject(this List<GObject> list, string name)
-        {
-            foreach (GObject child in list)
-            {
-                if (child.Name == name)
-                {
-                    return child;
-                }
             }
             return null;
         }
